27e772318ae2098d094e1e9c33d945dfd629294e
WillyWonkaCocoa <wgao1@MacBook-Pro.local>
2016-02-05 20:53:30 -0500

    Added all required user stories
    added pull to refresh

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by wgao1 on 2/2/16.
+//  Copyright © 2016 William Gao. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
